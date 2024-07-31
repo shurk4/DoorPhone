f3e6450baf67dd86d788fbf92a6141080460fa6a
Aleksandr Pryadiev <59886241+shurk4@users.noreply.github.com>
2024-07-31 16:29:56 +0300

    Call sounds added

---
+++ Panel/callplayer.h
@@ -0,0 +1,27 @@
+#ifndef CALLPLAYER_H
+#define CALLPLAYER_H
+
+#include <QObject>
+#include <QMediaPlayer>
+#include <QMediaPlaylist>
+#include <QThread>
+
+class CallPlayer : public QObject
+{
+    Q_OBJECT
+
+    QMediaPlayer *player;
+    QMediaPlaylist *playlist;
+
+public slots:
+    void run();
+    void start();
+    void stop();
+
+public:
+    CallPlayer();
+
+    void setTrackIndex(int idx);
+};
+
+#endif // CALLPLAYER_H
